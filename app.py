362b4c5e0bea4e253d893be09c27b79ea4ca69d3
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2018-10-25 17:30:43 -0400

    Agregando una parte del token

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
 # Resources
-from resources.Users import UserList, User, Login
+from resources.Users import UserList, User, UserLogin, SecretResource, UserLogoutAccess, UserLogoutRefresh, TokenRefresh, UserName
 from resources.Roles import RoleList, Role
 
 # instantiate the app
 app = Flask(__name__)
 #app.config.from_object(config)
 app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 jwt = JWTManager(app)
 api = Api(app)
 
@@ -22,11 +24,21 @@ CORS(app)
 # users route
 api.add_resource(UserList, '/user')
 api.add_resource(User, '/user/<user_id>')
-api.add_resource(Login, '/auth')
+api.add_resource(UserName, '/user/<username>')
+api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogoutAccess, '/logout/access')
+api.add_resource(UserLogoutRefresh, '/logout/refresh')
+api.add_resource(TokenRefresh, '/token/refresh')
+api.add_resource(SecretResource, '/secret')
 
 # roles route
 api.add_resource(RoleList, '/role')
 api.add_resource(Role, '/role/<role_id>')
+
+#@jwt.token_in_blacklist_loader
+#def check_if_token_in_blacklist(decrypted_token):
+#    jti = decrypted_token['jti']
+#    return is_jti_blacklisted(jti)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- resources/Users.py
+++ resources/Users.py
@@ -1,4 +1,4 @@
-from flask_restful import abort
+from flask_restful import abort, Resource
 import simplejson as json
 from pymysql import DatabaseError
 from textwrap import dedent
@@ -9,6 +9,10 @@ from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_r
 class UserList(BaseRes):
 	database = "PRUEBA"
 	table = "USER"
+
+	@staticmethod
+	def generate_hash(password):
+		return sha256.hash(password)
 
 	def get(self):
 		try:
@@ -31,6 +35,7 @@ class UserList(BaseRes):
 			print(user)
 			del user['id']
 			del user['name']
+			user['password'] = self.generate_hash(user['password'])
 			self.insert('USER', user)
 			#result = self.queryOne("SELECT TOP 1 * FROM USER ORDER BY ID DESC")
 			result = self.queryOne("SELECT * FROM USER ORDER BY ID DESC LIMIT 1")
@@ -42,6 +47,8 @@ class UserList(BaseRes):
 			abort(500, message="{0}: {1}".format(e.__class__.__name__, e.__str__()))
 		
 		return json.dumps(result), 201, { 'Access-Control-Allow-Origin': '*' }
+
+	
 
 class User(BaseRes):
 	database = "PRUEBA"
@@ -79,11 +86,10 @@ class User(BaseRes):
 
 	def delete(self, user_id):
 		try:
-			print(user_id)
+			#print(user_id)
 			result = self.queryOne("SELECT * FROM USER WHERE ID = %s", [user_id])
-			print(result)
+			#print(result)
 			if result is None:
-				print("Entro")
 				abort(404, message="Resource {} doesn't exists".format(user_id))
 			else:
 				self.remove("DELETE FROM USER WHERE ID = %s", [user_id])
@@ -96,8 +102,12 @@ class User(BaseRes):
 
 		return json.dumps(result), 204, { 'Access-Control-Allow-Origin': '*' }
 
+	@classmethod
+	def is_jti_blacklisted(self, jti):
+		query = self.queryOne("SELECT * FROM revoked_token WHERE jti = %s", [jti])
+		return bool(query)
 
-class Login(BaseRes):
+class UserLogin(BaseRes):
 	database = "PRUEBA"
 	table = "USER"
 
@@ -105,28 +115,66 @@ class Login(BaseRes):
 		try:
 			user = self.parser.parse_args()
 			print(user)
-			result = self.queryOne("SELECT * FROM USER WHERE EMAIL = %s", [user['email']])
-			if not result:
-				abort(404, message="User {} doesn't exists".format(user['email']))
+			result = self.queryOne("SELECT * FROM USER WHERE USERNAME = %s", [user['username']])
+			print(result)
+			if result is None:
+				return json.dumps({ 'message': 'Invalid credentials', 'authenticated': False }), 404
 			#if self.verify_hash(result['password'], user['password']):
-			if result['password'] != user['password']:
-				#access_token = create_access_token(identity = user['email'])
-				#refresh_token = create_refresh_token(identity = user['email'])
-				abort(404, message="Acceso Prohibido {} doesn't exists".format(user['id']))
-			#else:
-				#abort(404, message="Acceso Prohibido {} doesn't exists".format(user['id']))
-		except DatabaseError as e:
-			self.rollback()
-			abort(500, message="{0}: {1}".format(e.__class__.__name__, e.__str__()))
+			if self.verify_hash(user['password'], result['password']):
+				access_token = create_access_token(identity = user['username'])
+				refresh_token = create_refresh_token(identity = user['username'])
+				return json.dumps({'user': result, 
+								'message': 'Inicio de session con el usuario {}'.format(result['username']),
+								'access_token': access_token,
+                				'refresh_token': refresh_token }), 201, { 'Access-Control-Allow-Origin': '*' }
+			else:
+				return {'message': 'Wrong credentials'}
 		except Exception as e:
 			abort(500, message="{0}: {1}".format(e.__class__.__name__, e.__str__()))
-		
-		return json.dumps(result), 201, { 'Access-Control-Allow-Origin': '*' }
-
-	@staticmethod
-	def generate_hash(password):
-		return sha256.hash(password)
+	
 
 	@staticmethod
 	def verify_hash(password, hash):
 		return sha256.verify(password, hash)
+
+
+class SecretResource(Resource):
+	@jwt_required
+	def get(self):
+		return {
+			'answer': 42
+		}
+
+class TokenRefresh(Resource):
+	@jwt_refresh_token_required 
+	def post(self): 
+		current_user = get_jwt_identity() 
+		access_token = create_access_token(identity = current_user) 
+		return {'access_token': access_token}
+    
+
+class UserLogoutAccess(Resource):
+	@jwt_required
+	def post(self):
+		jti = get_raw_jwt()['jti']
+		try:
+			revoked_token = {'jti': jti}
+			self.insert('revoked_token', revoked_token)
+			self.commit()
+			return {'message': 'Access token has been revoked'}
+		except:
+			return {'message': 'Something went wrong'}, 500
+
+class UserLogoutRefresh(Resource):
+	@jwt_refresh_token_required
+	def post(self):
+		jti = get_raw_jwt()['jti']
+		try:
+			revoked_token = {'jti': jti}
+			self.insert('revoked_token', revoked_token)
+			self.commit()
+			return {'message': 'Access token has been revoked'}
+		except:
+			return {'message': 'Something went wrong'}, 500
+
+	
